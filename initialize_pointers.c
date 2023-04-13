9a9fc033c5185fd71eb50d504841e5a9278622fb
Tesfaye Meberate Anteneh <126094073+tesfayeanteneh@users.noreply.github.com>
2023-04-13 19:00:35 +0300

    Update initialize_pointers.c

--- initialize_pointers.c
+++ initialize_pointers.c
@@ -4,6 +4,8 @@ int main()
 {
     printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
     int *ptr = NULL;    // ptr is a NULL pointer
+    
+    int *ptr = (int*)0;
 
     printf("\n\n The value of ptr is: %x ", ptr);
     return 0;
